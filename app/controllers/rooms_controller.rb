33af7e895ea492953692e2e35522edd60802059a
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-02-02 18:15:00 +0200

    room show page

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -6,5 +6,23 @@ class RoomsController < ApplicationController
   end
 
   def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.create room_params
+  end
+
+  def show
+    @room = Room.find params[:id]
+    @rooms = Room.public_rooms
+    @users = User.all_except current_user
+    render :index
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name)
   end
 end
